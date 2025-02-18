b5bc5ef5f7b4755e15d22fe880d08fd3eb95cd51
lecopivo <skrivantomas@seznam.cz>
2025-02-18 02:16:19 +0100

    intial commit

---
+++ CompiledMathlib.lean
@@ -0,0 +1,4 @@
+-- This module serves as the root of the `CompiledMathlib` library.
+-- Import modules here that should be built as part of the library.
+import CompiledMathlib.Basic
+import Mathlib
---
+++ lakefile.lean
@@ -0,0 +1,11 @@
+import Lake
+open Lake DSL
+
+package "CompiledMathlib" where
+  precompileModules := true
+
+@[default_target]
+lean_lib «CompiledMathlib» where
+
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.16.0"
+
